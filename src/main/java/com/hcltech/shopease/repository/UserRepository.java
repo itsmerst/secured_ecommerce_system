6a65944e3f514a82d22f16e24e1d2be4ae307e34
Rutik Sambhaji Tonpe <rutiktonpe@gmail.com>
2025-10-24 11:39:25 +0530

    Update UserRepository.java

--- src/main/java/com/hcltech/shopease/repository/UserRepository.java
+++ src/main/java/com/hcltech/shopease/repository/UserRepository.java
@@ -3,4 +3,9 @@ package com.hcltech.shopease.repository;
 import com.hcltech.shopease.model.User;
 import org.springframework.data.jpa.repository.JpaRepository; import java.util.Optional;
 
-public interface UserRepository extends JpaRepository<User, Long> { Optional<User> findByUsername(String username); boolean existsByUsername(String username); boolean existsByEmail(String email); }
+public interface UserRepository extends JpaRepository<User, Long> { Optional<User> findByUsername(String username); 
+                                                                   boolean existsByUsername(String username); 
+                                                                   boolean existsByEmail(String email); 
+                                                                   List<Purchase> findByUserId(Long userId);
+
+                                                                  }
